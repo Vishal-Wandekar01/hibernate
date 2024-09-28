85be86efc9108fb96123316add79a13232675667
Vishal Wandekar <vishalwandekar@gmail.com>
2024-09-28 19:27:29 +0530

    Github test

--- ProjectWithMaven/src/main/java/com/tut/App.java
+++ ProjectWithMaven/src/main/java/com/tut/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Hibernate!" );
+        System.out.println( "Hello Hibernate!.... This is Techmonk" );
     }
 }
