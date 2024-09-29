17103731f6bfe5985abde96a363b10eedbd15053
Vishal Wandekar <vishalwandekar@gmail.com>
2024-09-29 10:51:47 +0530

    Created resource folder and move hibernate.cfg.xml in that Created factory object to fetch all the data from database

--- ProjectWithMaven/src/main/java/com/tut/App.java
+++ ProjectWithMaven/src/main/java/com/tut/App.java
@@ -1,13 +1,26 @@
 package com.tut;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Hibernate!.... This is Techmonk" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello Hibernate!.... This is Techmonk");
+
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml"); // if it is not getting fine name so need to mantioned manually
+		SessionFactory factory = cfg.buildSessionFactory();
+
+		// SessionFactory factory = new
+		// Configuration().configure().buildSessionFactory();
+
+		System.out.println(factory);
+//         factory.getCurrentSession();
+		
+		System.out.println(factory.isClosed());
+	}
 }
