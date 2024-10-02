4fbc8baf0192c511ce85ef29048c77d0628b1fd4
Vishal Wandekar <vishalwandekar@gmail.com>
2024-10-02 11:19:53 +0530

    -Created separate Student class, used all basics Hibernate Annotations -Save image in database

--- ProjectWithMaven/src/main/java/com/tut/App.java
+++ ProjectWithMaven/src/main/java/com/tut/App.java
@@ -1,5 +1,9 @@
 package com.tut;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -10,7 +14,7 @@ import org.hibernate.cfg.Configuration;
  *
  */
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Hello Hibernate!.... This is Techmonk");
 
 		Configuration cfg = new Configuration();
@@ -28,15 +32,34 @@ public class App {
 		System.out.println(std);
 		
 		
+		//creating Address
+		Address ad = new Address();
+		ad.setStreet("Street1 ");
+		ad.setCity("DELHI");
+		ad.setOpen(true);
+		ad.setAddedDate(new Date());
+		ad.setX(1234.123);
+		
+		//read image
+		FileInputStream fis = new FileInputStream("src/main/java/Chiku.jpg");
+		byte[] data = new byte[fis.available()];
+		fis.read(data);
+		ad.setImage(data);
+		
+		
 		Session session = factory.openSession();
 		
 		Transaction tx = session.beginTransaction();
 		
 		session.save(std);
+		session.save(ad);//will save in database
 		
 		tx.commit();
 		
 		session.close();
+		
+	
+		System.out.println("Done.. ");
 		
 	}
 }
